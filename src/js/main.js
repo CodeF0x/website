9be00f7de63f06bf33138e278cd1d6f07308a2fc
toettl <toettl@united-internet.de>
2019-10-21 13:51:04 +0200

    Dark mode & responsive.

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,7 @@
-function checkVisible(elm) {
-  var rect = elm.getBoundingClientRect();
-  var viewHeight = Math.max(
-    document.documentElement.clientHeight,
-    window.innerHeight
-  );
-  return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
-}
-
-document.getElementById('arrow-down').addEventListener('click', function() {
-  this.classList.add('fade-out');
-  setTimeout(() => {
-    this.style.display = 'none';
-  }, 1000);
-  document.getElementById('two').scrollIntoView({ behavior: 'smooth' });
-});
-
-document.addEventListener('scroll', () => {
-  const two = document.getElementById('two');
-  if (checkVisible(two)) {
-    const arrow = document.getElementById('arrow-down');
-    arrow.classList.add('fade-out');
-    setTimeout(() => {
-      arrow.style.display = 'none';
-    }, 1000);
-  }
-});
+document
+  .getElementById('mobile-header-expand')
+  .addEventListener('click', () => {
+    const links = document.getElementById('mobile-header-links');
+    const style = window.getComputedStyle(links).display;
+    links.style.display = style === 'none' ? 'block' : 'none';
+  });
