1589bd312bb039f33e4629c472bc955c0c673d74
CodeF0x <contact@codef0x.dev>
2020-04-19 23:45:06 +0200

    Deleted old files and working on index page.

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-document
-  .getElementById('mobile-header-expand')
-  .addEventListener('click', () => {
-    const links = document.getElementById('mobile-header-links');
-    const style = window.getComputedStyle(links).display;
-    links.style.display = style === 'none' ? 'block' : 'none';
-  });
